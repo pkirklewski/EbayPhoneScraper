3b8819e2b29a789b43a4c0524cb99263cbf565b4
pkirklewski <kirklewskis@gmail.com>
2017-11-17 12:14:07 +0000

    First commit - Why is there no References. Why can I not use System.Net.WebCLient ???

---
+++ TheEbayProject/EbayScraper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace TheEbayProject
+{
+    public class EbayScraper
+    {
+        System.Net.WebClient client = new System.Net.WebClient();
+        String htmlCode = client.DownloadString("https://www.ebay.co.uk/b/Samsung-Galaxy-S8/9355/bn_75787856?LH_ItemCondition=2000");
+        
+    }
+}
